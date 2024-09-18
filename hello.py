c347641095804054233ebf19ddc8b87d22bee925
Deokkyu <ydg0426@naver.com>
2024-09-18 21:02:18 +0900

    Create navBar && Create InputForm with wtF

--- hello.py
+++ hello.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 #Create a Flask Instance
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "my super secret that no one is "
+#이 부분은 Flask APP의 보안을 위해 ***비밀키***를 설정하는 부분
+#세션 암호화 or CSRF 보호를 위해 사용된다.
+
+class NamerForm(FlaskForm): #Create a Form Class
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    #name 필드는 텍스트 입력을 받는 필드이다
+    #validators는 이 필드가 반드시 채워져야 한다는 유효성 검사를 설정
+    submit = SubmitField("Submit")
+    #폼을 제출할 떄 사용하는 버튼
+    #SubmitField는 Submit이라는 버튼을 생성해준다
+
+
 
 # Create a route decorator__URL~~~.HTML
 # render_template 를 통해서 html파일 불러오기
@@ -43,4 +59,20 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template("500.html"), 500
 
+
+
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm() #Form 객체를 생성, 
+    #이 폼은 템플릿에서 사용자에게 이름을 입력받는 역할을 한다
+    # Validate Form
+    if form.validate_on_submit():# 폼이 제출되었고 유효한지 확인합니다//폼이 입력시 다시 함수가 돌아감
+        name = form.name.data #폼이 제출되면 name 이라는 변수에 form.name.data 값이 들어감
+        form.name.data = '' #그리고 다시 nULL로 초기화 시킴
+        
+    return render_template("name.html",
+                           name = name,
+                           form = form)
 app.run(debug=True)
